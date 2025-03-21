7a5c9fe61829d4d0583129cf0e7cdba9e60910a1
Scott Harley <sandflatsharley@gmail.com>
2025-03-21 18:32:04 +0200

    Capitalize 'Stock Moved' in transaction display

--- www/js/animals.js
+++ www/js/animals.js
@@ -370,7 +370,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             transactionItem.innerHTML = `
                 <div class="transaction-details">
                     <span class="transaction-date">${date}</span>
-                    <span class="transaction-category">${transaction.type === 'move' ? 'stock moved' : transaction.category}</span>
+                    <span class="transaction-category">${transaction.type === 'move' ? 'Stock Moved' : transaction.category}</span>
                     <span class="transaction-description">${description}</span>
                     ${priceInfo}
                 </div>
